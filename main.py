5344f3dba7904624cbba5a01394b055846fbc7fb
zeviplex <130025165+zeviplex@users.noreply.github.com>
2023-09-20 12:46:06 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+from pyrogram import Client, filters
+
+
+API_ID = "29501536"
+API_HASH = "cdf38d8032a49497f3126caa51e72045"
+BOT_TOKEN = "6437402376:AAEnpZt8oFZ7FIGNxhamEj-FOmo2FtLSLH4"
+
+zeviai = Client(
+  name="ZeviAi",
+  api_id=29501536,
+  api_hash=cdf38d8032a49497f3126caa51e72045,
+  bot_token=6437402376:AAEnpZt8oFZ7FIGNxhamEj-FOmo2FtLSLH4
+)
+
+
+
+print("bot Was Started")
+
+zeviai.run()
