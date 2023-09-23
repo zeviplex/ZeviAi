56664a7fe03f05d0f1acbd66778428de6a06423e
zeviplex <130025165+zeviplex@users.noreply.github.com>
2023-09-23 16:10:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,58 @@
-import telegram
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
+import os
+from telegram.ext import Updater, MessageHandler, CommandHandler
 
-# Replace 'YOUR_BOT_TOKEN' with your actual bot token
-bot_token = '6437402376:AAEnpZt8oFZ7FIGNxhamEj-FOmo2FtLSLH4'
+# Define a knowledge base of Ayurvedic medicines
+ayurvedic_medicines = {
+    'neem': 'Neem is known for its antibacterial properties and is used to treat various skin conditions and infections.',
+    'turmeric': 'Turmeric has anti-inflammatory and antioxidant properties and is used to improve digestion and reduce inflammation.',
+    'ashwagandha': 'Ashwagandha is an adaptogenic herb that helps reduce stress and anxiety and improve overall vitality.',
+    # Add more Ayurvedic medicines and their descriptions here
+}
 
-# Initialize the Telegram Bot
-bot = telegram.Bot(token=bot_token)
-
-# Function to welcome users
+# Define a function to start the bot
 def start(update, context):
-    user_name = update.message.from_user.first_name
-    context.bot.send_message(chat_id=update.message.chat_id, text=f"Hello, {user_name}! Welcome to the Ayurvedic Medicine Bot. How can I assist you today?")
+    user = update.message.from_user
+    update.message.reply_text(f"Hello, {user.first_name}! How can I assist you today?")
 
-# Function to provide answers about Ayurvedic medicine
-def answer_question(update, context):
-    # You can implement the logic to answer Ayurvedic medicine questions here
-    # For example, you can use a dictionary with predefined answers
-    answers = {
-        "What is Ayurveda?": "Ayurveda is a traditional system of medicine that originated in India...",
-        "How can I improve digestion?": "To improve digestion in Ayurveda, you can try...",
-        # Add more questions and answers as needed
-    }
-
-    question = update.message.text
-    if question.lower() in ["hi", "hello"]:
-        context.bot.send_message(chat_id=update.message.chat_id, text="Hello! How can I assist you today?")
-    elif question in answers:
-        context.bot.send_message(chat_id=update.message.chat_id, text=answers[question])
+# Define a function to search the knowledge base
+def search(update, context):
+    user_message = update.message.text.lower()
+    
+    if user_message in ayurvedic_medicines:
+        medicine_description = ayurvedic_medicines[user_message]
+        update.message.reply_text(f"Here's information about {user_message}: {medicine_description}")
     else:
-        context.bot.send_message(chat_id=update.message.chat_id, text="I'm sorry, I don't have an answer for that question. Please ask something else.")
+        update.message.reply_text("I'm sorry, I couldn't find information about that. Please ask about an Ayurvedic medicine.")
 
-# Create an Updater for the bot
-updater = Updater(token=bot_token, use_context=True)
+# Define a function to handle unknown commands
+def unknown(update, context):
+    update.message.reply_text("I'm not sure what you mean. Please type a valid command or ask about an Ayurvedic medicine.")
 
-# Get the dispatcher to register handlers
-dispatcher = updater.dispatcher
+def main():
+    # Initialize your bot with your Telegram bot token from Render's environment variables
+    bot_token = os.getenv("BOT_TOKEN")
+    if not bot_token:
+        raise ValueError("BOT_TOKEN not found in environment variables.")
+    
+    updater = Updater(token=bot_token, use_context=True)
+    dispatcher = updater.dispatcher
 
-# Register the start command handler
-dispatcher.add_handler(CommandHandler('start', start))
+    # Define command handlers
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
 
-# Register the message handler to answer questions
-dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, answer_question))
+    # Define message handler to search for Ayurvedic medicine information
+    message_handler = MessageHandler(Filters.text & ~Filters.command, search)
+    dispatcher.add_handler(message_handler)
 
-# Start the bot
-updater.start_polling()
+    # Define a handler for unknown commands
+    unknown_handler = MessageHandler(Filters.command, unknown)
+    dispatcher.add_handler(unknown_handler)
 
-# Run the bot until Ctrl+C is pressed
-updater.idle()
+    # Start the bot
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
+    
