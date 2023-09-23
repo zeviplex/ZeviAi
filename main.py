c138aef74c99819dddf855004fb8a31057a56830
zeviplex <130025165+zeviplex@users.noreply.github.com>
2023-09-23 14:43:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,49 @@
-from pyrogram import Client, filters
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
 
+# Replace 'YOUR_BOT_TOKEN' with your actual bot token
+bot_token = '6437402376:AAEnpZt8oFZ7FIGNxhamEj-FOmo2FtLSLH4'
 
-API_ID = "29501536"
-API_HASH = "cdf38d8032a49497f3126caa51e72045"
-BOT_TOKEN = "6437402376:AAEnpZt8oFZ7FIGNxhamEj-FOmo2FtLSLH4"
+# Initialize the Telegram Bot
+bot = telegram.Bot(token=bot_token)
 
-zeviai = Client(
-  name="ZeviAi",
-  api_id=29501536,
-  api_hash=cdf38d8032a49497f3126caa51e72045,
-  bot_token=6437402376:AAEnpZt8oFZ7FIGNxhamEj-FOmo2FtLSLH4
-)
+# Function to welcome users
+def start(update, context):
+    user_name = update.message.from_user.first_name
+    context.bot.send_message(chat_id=update.message.chat_id, text=f"Hello, {user_name}! Welcome to the Ayurvedic Medicine Bot. How can I assist you today?")
 
+# Function to provide answers about Ayurvedic medicine
+def answer_question(update, context):
+    # You can implement the logic to answer Ayurvedic medicine questions here
+    # For example, you can use a dictionary with predefined answers
+    answers = {
+        "What is Ayurveda?": "Ayurveda is a traditional system of medicine that originated in India...",
+        "How can I improve digestion?": "To improve digestion in Ayurveda, you can try...",
+        # Add more questions and answers as needed
+    }
 
+    question = update.message.text
+    if question.lower() in ["hi", "hello"]:
+        context.bot.send_message(chat_id=update.message.chat_id, text="Hello! How can I assist you today?")
+    elif question in answers:
+        context.bot.send_message(chat_id=update.message.chat_id, text=answers[question])
+    else:
+        context.bot.send_message(chat_id=update.message.chat_id, text="I'm sorry, I don't have an answer for that question. Please ask something else.")
 
-print("bot Was Started")
+# Create an Updater for the bot
+updater = Updater(token=bot_token, use_context=True)
 
-zeviai.run()
+# Get the dispatcher to register handlers
+dispatcher = updater.dispatcher
+
+# Register the start command handler
+dispatcher.add_handler(CommandHandler('start', start))
+
+# Register the message handler to answer questions
+dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, answer_question))
+
+# Start the bot
+updater.start_polling()
+
+# Run the bot until Ctrl+C is pressed
+updater.idle()
